8956ef94625e889a134a44b2349c590e7a836253
RahulRST <srahuljayaraj@gmail.com>
2023-09-04 22:01:58 +0530

    tailwind added and login page modifications

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Login = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!name || !email) {
-      setError('Please fill in all fields');
+      setError("Please fill in all fields");
       return;
     }
 
-    setName('');
-    setEmail('');
-    setError('');
+    setName("");
+    setEmail("");
+    setError("");
   };
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-white p-8 rounded shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+      <div className="p-8 rounded shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Debug Bingo</h2>
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Name:
             </label>
             <input
@@ -34,12 +37,15 @@ const Login = () => {
               name="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+              className="input input-bordered"
               required
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Email:
             </label>
             <input
@@ -48,14 +54,11 @@ const Login = () => {
               name="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+              className="input input-bordered"
               required
             />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
-          >
+          <button type="submit" className="btn btn-primary">
             Login
           </button>
         </form>
